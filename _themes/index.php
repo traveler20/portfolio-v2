b14781019f08104c0b5b418d3f25fbc6938eb251
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2021-10-19 14:20:54 +0900

    🚨Tests WordPress

---
+++ _themes/index.php
@@ -0,0 +1,251 @@
+        get_header()
+
+		<!-- main -->
+		<main>
+			<!-- .p-fv -->
+			<section class="p-fv">
+				<div class="c-inner">
+					<article class="p-fv__visual"></article>
+					<h1 class="p-fv__title js-loading">
+						<span id="js-fvTitle1">Solving</span>
+						<span id="js-fvTitle2">with&nbsp;Design</span>
+					</h1>
+					<a href="#about" id="js-fvScroll" class="p-fv__scroll">scroll</a>
+				</div>
+				<ul class="p-fv__circles">
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</section>
+			<!-- /.p-fv -->
+
+			<!-- .p-about c-section -->
+			<section class="p-about c-section" id="about">
+				<div class="c-section__inner">
+					<h2 class="c-section__title js-fadeIn" data-en="about">理念</h2>
+					<dl class="p-about__content">
+						<dt>Vision</dt>
+						<dd class="js-fadeIn js-innerspan">デザインで世の中をよくする</dd>
+					</dl>
+					<dl class="p-about__content">
+						<dt>Mission</dt>
+						<dd class="js-fadeIn">
+							<span class="u-inline-block">潜在的な課題を発見し</span
+							><span class="u-inline-block">デザインで解決する</span>
+							<p>
+								ただデザイン・コーディングするだけではなく潜在的な課題を仮説立てて解決するよう貢献します。
+							</p>
+						</dd>
+					</dl>
+				</div>
+			</section>
+			<!-- /.p-about c-section -->
+
+			<!-- .p-work c-section -->
+			<section class="p-work c-section" id="work">
+				<div class="c-section__inner">
+					<h2 class="c-section__title js-fadeIn" data-en="work">制作実績</h2>
+					<article class="p-work__content">
+						<h3 class="c-section__subtitle js-fadeIn" data-en="Web design">
+							<span>コーディング/デザイン</span>
+						</h3>
+						<ul class="p-work__lists">
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+						</ul>
+						<h3 class="c-section__subtitle js-fadeIn" data-en="Others">
+							<span>その他制作実績</span>
+						</h3>
+						<ul class="p-work__lists">
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+							<li>
+								<img
+									src="https://traveler20.site/portfolio/asset/img/works/cording_lp-plant.webp"
+									alt=""
+									loading="lazy"
+								/>
+							</li>
+						</ul>
+					</article>
+
+					<p class="p-work__description">
+						その他、印刷通販サイトでのコーディング経験やスタートアップ企業のランディングページ制作・運用経験があります。
+					</p>
+					<div class="c-button">
+						<h3>詳細はお問い合わせください。</h3>
+						<a href="#contact">Contact</a>
+					</div>
+				</div>
+			</section>
+			<!-- /.p-work c-section -->
+
+			<!-- .p-service c-section -->
+			<section class="p-service c-section" id="service">
+				<div class="c-section__inner">
+					<h2 class="c-section__title js-fadeIn" data-en="service">事業内容</h2>
+					<figure class="p-service__figure js-fadeIn">
+						<img
+							src="https://traveler20.site/portfolio/asset/img/works/design_hp-beauty.webp"
+							alt=""
+							loading="lazy"
+						/>
+						<figcaption class="p-service__figcaption">
+							<h3 class="c-section__subtitle js-fadeIn" data-en="Web design">
+								<span>サイトデザイン・運用事業</span>
+							</h3>
+							<p>
+								WEBサイトのデザインからコーディング、WordPressの組み込みやサーバーへのアップまで一貫した業務が可能です。<br />
+							</p>
+						</figcaption>
+					</figure>
+					<figure class="p-service__figure js-fadeIn">
+						<img
+							src="https://traveler20.site/portfolio/asset/img/works/design_hp-beauty.webp"
+							alt=""
+							loading="lazy"
+						/>
+						<figcaption class="p-service__figcaption">
+							<h3 class="c-section__subtitle js-fadeIn" data-en="Development">
+								<span>メディア運営・制作</span>
+							</h3>
+							<p>
+								WordPressを用いたメディアサイトの運営やノーコードによるアプリ開発を行っています。<br />
+							</p>
+						</figcaption>
+					</figure>
+				</div>
+			</section>
+			<!-- /.p-service c-section -->
+
+			<!-- .p-profile c-section -->
+			<section class="p-profile c-section" id="profile">
+				<div class="c-section__inner">
+					<h2 class="c-section__title js-fadeIn" data-en="profile">
+						自分について
+					</h2>
+					<figure class="p-profile__figure">
+						<picture class="p-profile__image">
+							<!-- <source srcset="./asset/img/sample.webp" type="image/webp" /> -->
+							<img
+								src="https://traveler20.site/portfolio/asset/img/businesscard-profile.png"
+								alt=""
+							/>
+						</picture>
+						<figcaption class="p-profile__figcaption">
+							<h3 class="c-section__subtitle js-fadeIn" data-en="Greeting">
+								<span>あいさつ</span>
+							</h3>
+							<p>WEB Developer. WEB Designer.</p>
+							<p>
+								WEBデザイナーのtraveler20といいます。コーディングを中心にデザイン、WEB制作、サーバー関係、プログラミング、SEO等を仕事で携わってきました。
+							</p>
+							<p>
+								ただLPやホームページをつくっておわりではなく、WEBサイトを継続的に運用しながら目的を達成するよう寄り添って考えることができますので、お気軽にご相談いただければと思います。
+							</p>
+							<ul>
+								<li><a href="https://www.facebook.com/O.traveler20/"></a></li>
+								<li><a href="https://www.wantedly.com/id/traveler20"></a></li>
+								<!-- <li><a href="">Portfolio</a></li> -->
+							</ul>
+						</figcaption>
+					</figure>
+				</div>
+			</section>
+			<!-- /.p-profile c-section -->
+
+			<!-- .p-contact c-section -->
+			<section class="p-contact c-section" id="contact">
+				<div class="c-section__inner">
+					<h2 class="c-section__title js-fadeIn" data-en="contact">
+						お問い合わせ
+					</h2>
+					<div class="p-contact__content"></div>
+				</div>
+			</section>
+			<!-- /.p-contact c-section -->
+		</main>
+		<!-- /main -->
+
+        get_footer()
