a307ad88480f7eb86589b8a723b86fb88d3a5455
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2021-12-28 16:42:36 +0900

    🚀UPDATE Original Themes

--- _wp-content/themes/traveler20/page.php
+++ _wp-content/themes/traveler20/page.php
@@ -6,8 +6,8 @@
 			<section class="p-page__fv">
 				<div class="c-inner">
 					<h1 class="p-page__fvTitle js-loading">
-						<span id="js-fvTitle1" class="p-page__fvTitleEn">Work</span>
-						<span id="js-fvTitle2" class="p-page__fvTitleJa">実績の一覧</span>
+						<span id="js-fvTitle1" class="p-page__fvTitleEn"><?php echo esc_attr($post->post_name); ?></span>
+						<span id="js-fvTitle2" class="p-page__fvTitleJa"><?php the_title(); ?></span>
 					</h1>
 				</div>
 			</section>
@@ -17,8 +17,12 @@
 			<section class="p-page__content c-section" id="content">
 				<div class="c-section__inner">
 					<!-- page.php -->
-                    <?php //固定ページ内容
-                    get_template_part('tmp/page-contents'); ?>
+					<?php if (have_posts()): ?>
+						<?php while (have_posts()) : the_post(); ?>
+						<?php the_content(); ?>
+						<?php endwhile; ?>
+					<?php else: ?>
+					<?php endif; ?>
 				</div>
 			</section>
 			<!-- /.p-page__content -->
--- _wp-content/themes/traveler20/single.php
+++ _wp-content/themes/traveler20/single.php
@@ -16,9 +16,14 @@
 			<!-- .p-page__content -->
 			<section class="p-page--single__content c-section" id="content">
 				<div class="c-section__inner">
+					<h1 class="p-page--single__title"><?php the_title(); ?></h1>
 					<!-- single.php -->
-                    <?php //投稿ページ内容
-                    get_template_part('tmp/single-contents'); ?>
+					<?php if (have_posts()): ?>
+						<?php while (have_posts()) : the_post(); ?>
+						<?php the_content(); ?>
+						<?php endwhile; ?>
+					<?php else: ?>
+					<?php endif; ?>
 				</div>
 			</section>
 			<!-- /.p-page__content -->
