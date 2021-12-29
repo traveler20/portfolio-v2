b58065be07ae2508fdb7f97cd83abab9cba032ed
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2021-12-29 13:58:30 +0900

    ✨Add 404 Page

---
+++ _wp-content/themes/traveler20/404.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+<!-- main -->
+<main class="p-page--404">
+    <!-- .p-page__fv -->
+    <section class="p-page__fv">
+        <div class="c-inner">
+            <h1 class="p-page__fvTitle js-loading">
+                <span id="js-fvTitle1" class="p-page__fvTitleEn">404</span>
+                <span id="js-fvTitle2" class="p-page__fvTitleJa">ページは見つかりませんでした</span>
+            </h1>
+        </div>
+    </section>
+    <!-- /.p-page__fv -->
+
+    <!-- .p-page__content -->
+    <section class="p-page__content c-section" id="content">
+        <div class="c-section__inner">
+            <!-- page.php -->
+            <p>当サイトをご覧頂きありがとうございます。</p>
+            <p>
+                申し訳ありませんが、アクセスしようとした記事は削除されたかURLが変更されています。
+            </p>
+            <div class="c-button">
+                <a href="/">トップページ</a>
+            </div>
+        </div>
+    </section>
+    <!-- /.p-page__content -->
+</main>
+<!-- /main -->
+
+<?php get_footer(); ?>
