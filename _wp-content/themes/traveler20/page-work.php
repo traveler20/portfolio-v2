c8fdf35b71d6e8bade12d68171ba44947acabd14
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2022-01-04 08:31:33 +0900

    ✨UPDATE post

--- _wp-content/themes/traveler20/page-work.php
+++ _wp-content/themes/traveler20/page-work.php
@@ -20,7 +20,7 @@
 					<!-- page.php -->
 						<?php
 							global $post;
-							$args = array( 'posts_per_page' => 8 );
+							$args = array( 'posts_per_page' => 9 );
 							$myposts = get_posts( $args );
 							foreach( $myposts as $post ) {
 							setup_postdata($post);
